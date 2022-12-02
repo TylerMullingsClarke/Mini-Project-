ee3bd57e8da60790c1a5160ae09dcd2b6f8bdc8e
james <james.grafton9@gmail.com>
2022-12-02 17:45:24 +0000

    merge sort comments

--- Mini Project/Mini Project/Model/MergeSorter.cs
+++ Mini Project/Mini Project/Model/MergeSorter.cs
@@ -9,19 +9,22 @@ namespace Mini_Project.Model
     public class MergeSorter : ISorter
     {
         public int[] Sort(int[] nums) {
+            //Basecase: An array of length 1 is sorted
             if (nums.Length == 1) { return nums; }
-            int splitIndex = (nums.Length / 2);
+            int splitIndex = (nums.Length / 2)-1;
+            //Splits array and sorts them
             int[] arrLeft = Sort(nums[0..splitIndex]);
             int[] arrRight = Sort(nums[(splitIndex+1)..nums.Length]); 
+            //Merges the sorted array
             int[] rtnArr = new int[arrLeft.Length + arrRight.Length];
             int i = 0; int j = 0;
             for (int k = 0; k < rtnArr.Length; k++)
             {
                 if (
-                    j >= arrRight.Length //adds from arr1 immediately if j is out of bounds
+                    j >= arrRight.Length //adds from arrLeft immediately if j is out of bounds
                     || (
-                        i < arrLeft.Length //adds from arr2 immediately if i is out of bounds
-                        && arrLeft[i] < arrRight[j] //then compares arr1 and arr2 if they are both in bounds
+                        i < arrLeft.Length //adds from arrRight immediately if i is out of bounds
+                        && arrLeft[i] < arrRight[j] //then compares arr1 and arrRight if they are both in bounds
                     )
                 )
                 {
